89f44d86e9a5ca4c01663c1a84782342af0dfec4
Leandro Cruz <leandro.saad@gmail.com>
2015-07-11 15:09:40 -0300

    Verifying is command can be executed. Added comments

--- design-patters/socialnetwork/server/src/main/java/sn/server/servlet/ApiServlet.java
+++ design-patters/socialnetwork/server/src/main/java/sn/server/servlet/ApiServlet.java
@@ -5,7 +5,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import sn.server.command.Command;
 import sn.server.command.CommandMapper;
+import sn.session.SessionManager;
 import xingu.container.Inject;
+import xingu.lang.SorryException;
 
 public class ApiServlet
 	extends SocialNetworkServletSupport
@@ -13,11 +15,45 @@ public class ApiServlet
 	@Inject
 	private CommandMapper mapper;
 	
+	@Inject
+	private SessionManager sessions;
+	
 	@Override
 	protected Object process(HttpServletRequest request, HttpServletResponse response)
 		throws Exception
 	{
-		Command command = mapper.translate(request);
-		return command.execute();
+		/*
+		 * TODO: Trabalho
+		 * 
+		 * Implementar as seguintes operaçoes com Chain of Responsability
+		 * 1. Verificar se o comando pode ser executado
+		 * 2. Logar a execucao do comando
+		 * 3. Verificar a presença de SQL Injection nos parametros antes da execucao do commando
+		 *
+		 */
+		boolean bypass = false;
+		long user = 0;
+		String path = request.getPathInfo();
+		switch(path)
+		{
+			case "/validate":
+			case "/auth":
+				bypass = true;
+				break;
+
+			default:
+				String token = request.getParameter("token");
+				user = sessions.userFor(token);
+				break;
+		}
+		if(bypass || user > 0)
+		{
+			Command command = mapper.translate(request);
+			return command.execute();
+		}
+		else
+		{
+			throw new SorryException("Sorry :)");
+		}
 	}
 }
